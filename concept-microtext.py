98f3218a9cdf38275ad3ae51f2343a1fe5ebcdf3
aalind0 <singh.aalind@gmail.com>
2018-06-19 03:47:34 +0800

    addition

--- concept-microtext.py
+++ concept-microtext.py
@@ -1,13 +1,14 @@
-from jellyfish import soundex,metaphone
+from jellyfish import soundex,metaphone,match_rating_codex
 
 sn = open("senticnet5.txt","r")
-lb = open("concept+metaphone.txt", "r+")
 for line in sn:
 	sndx = []
 	metaphn = []
+	codex = []
 	concept = (line.split('\t')[0])
 	words = concept.split('_')
 	for i in range(len(words)):
 		sndx.append(soundex(words[i]))
 		metaphn.append(metaphone(words[i]))
+		codex.append(match_rating_codex(words[i]))
 	print (concept,'\t','_'.join(metaphn))
--- script.py
+++ script.py
@@ -1,20 +1,34 @@
 import re
-from jellyfish import soundex,metaphone
+from jellyfish import soundex,metaphone,match_rating_codex
 
-sentic = open("concepts+soundex.txt", "r").read()
+sentic = open("codex.txt", "r").read()
+sentic1 = open("concepts+soundex.txt", "r").read()
+sentic2 = open("concepts+metaphone.txt", "r").read()
 #text = sentic.read().strip().split()
 string = input("Enter a string: ")
-sndx = soundex(string)
-print(sndx)
+cdx = match_rating_codex(string)
+print(cdx)
+sdx = soundex(string)
+meta = metaphone(string)
 
-
+print("Codex Results\n")
 for line in sentic.split("\n"):
     #print (line)
-    if sndx in line.split(" \t "):
+    if cdx in line.split(" \t "):
         print (line)
         #if sndx in soundx and len(sndx) == len(soundx):
         #    print(soundx)
+print ("Soundex Results\n")
 
+for line in sentic1.split("\n"):
+    if sdx in line.split(" \t "):
+        print (line)
+
+print("Metaphone Results\n")
+
+for line in sentic2.split("\n"):
+    if meta in line.split(" \t "):
+        print(line)
 # for line in text:
 #     if re.search(sndx, line, re.I):
 #         print(line)
